e88044f24fee883401e6040ee46e58fa283414df
SeonHyungJo <seonhyung.jo@gmail.com>
2019-09-27 09:58:51 +0900

    :palm_tree: SeonHyungJo / Remove remark

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
 module.exports = app => {
-  // app.on('pull_request.opened', async context => {
-  //   console.log('test', context)
-  //   return context.github.issues.createComment('')
-  // })
   app.on('pull_request.opened', async context => {
     const filesChanged = await context.github.pullRequests.listFiles(context.issue())
-    console.log('========================result======================', filesChanged.data)
 
     const urlList = await filesChanged.data.reduce((acc, cur) => {
       if (cur['filename'].match(/\.(md|markdown)$/)) {
@@ -17,14 +8,7 @@ module.exports = app => {
       }
       return acc
     }, '')
-    // if (results && results.length > 0) {
-    //   // make URLs
-    //   let urls = ''
-    //   await results.forEach(async (result) => {
-    //     urls += `\n[View rendered ${result.filename}](${context.payload.pull_request.head.repo.html_url}/blob/${context.payload.pull_request.head.ref}/${result.filename})`
-    //   })
-    // await context.github.pullRequests.update(context.issue({ body: `${context.payload.pull_request.body}\n\n-----${urls}` }))
+
     await context.github.pullRequests.update(context.issue({ body: urlList }))
-    // }
   })
 }
