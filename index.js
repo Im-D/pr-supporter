88a0c23b7559bc211c877ff5ddf4bea2cefff276
SeonHyungJo <seonhyung.jo@gmail.com>
2019-09-27 21:40:03 +0900

    :palm_tree: SeonHyungJo / Create Github Action

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 const path = require('path')
 
-function fileLink (pullRequest, file) {
+function fileLink(pullRequest, file) {
   return pullRequest.head.repo.html_url + path.join('/blob', pullRequest.head.ref, file.filename)
 }
-module.exports = app => {
-  app.on('pull_request.opened', async context => {
-    try {
-      const { owner, repo, number } = context.issue()
-      const filesChanged = await context.github.pullRequests.listFiles({ owner, repo, pull_number: number })
 
-      const urlList = await filesChanged.data.reduce((acc, cur) => {
-        if (cur.filename.match(/\.(md|markdown)$/)) {
-          const link = fileLink(context.payload.pull_request, cur)
-          acc += `[${cur.filename}](${link})\n`
-        }
-        return acc
-      }, '')
+// https://octokit.github.io/rest.js/
+async function run() {
+  try {
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(payload)
+    const { owner, repo, number } = payload
+    const filesChanged = github.pulls.listFiles({ owner, repo, pull_number : number })
 
-      await context.github.pullRequests.update(context.issue({ body: urlList }))
-    } catch (error) {
+    const urlList = await filesChanged.data.reduce((acc, cur) => {
+      if (cur.filename.match(/\.(md|markdown)$/)) {
+        // const link = fileLink(context.payload.pull_request, cur)
 
-    }
-  })
+        // acc += `[${cur.filename}](${link})\n`
+      }
+      return acc
+    }, '')
+
+    console.log('urlList', urlList);
+    github.pulls.update({ owner, repo, pull_number:number, body: urlList })
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
