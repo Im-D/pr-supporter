0bd5c5e779aa777b18f53234e75550921fab29c4
SeonHyungJo <seonhyung.jo@gmail.com>
2019-09-29 00:34:37 +0900

    :palm_tree: SeonHyungJo / Modify action/github

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ async function run() {
       .then((fileList) => 
         fileList.data.reduce((acc, cur) => {
           if (cur.filename.match(/\.(md|markdown)$/)) {
-            const link = fileLink(payload.pull_request, cur)
-            acc += `[${cur.filename}](${link})\n`
+            // const link = fileLink(payload.pull_request, cur)
+            acc += `[${cur.filename}](https://github.com/)\n`
           }
           return acc
         }, '')
